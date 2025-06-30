b8ef5fc6e27a8ddf97b1d2ef78a367f5c5935c21
Batubo Victory <victorybatubo76@gmail.com>
2025-06-30 14:44:48 +0100

    Add delete button feature

--- src/js/script.js
+++ src/js/script.js
@@ -4,18 +4,28 @@ const task = document.querySelector('#task');
 
 const addTask = document.querySelector('#addBtn');
 
-const list = document.querySelector('#list')
+const list = document.querySelector('#list');
 
 addBtn.addEventListener('click', e => {
   e.preventDefault(); //prevent default form behaviour
 
-  const value = task.value; // get task input
-  
-  let newList = document.createElement('li'); //create new list
+  let value = task.value; // get task input
+  const newList = document.createElement('li'); //create new list
+
+const delBtn = document.createElement('button');
+
+  delBtn.textContent = "Delete";
 
   newList.textContent = value;
+
+  newList.append(delBtn)
 
   list.append(newList);
 
   task.value = ''; //reset input value
+
+  delBtn.addEventListener('click', e => {
+  newList.remove();
 })
+})
+
