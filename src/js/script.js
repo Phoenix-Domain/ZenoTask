9cf3f8202571960cde7782f2a23ca92185a57975
Batubo Victory <victorybatubo76@gmail.com>
2025-06-30 15:00:36 +0100

    Style delete buttons

--- src/js/script.js
+++ src/js/script.js
@@ -9,12 +9,16 @@ const list = document.querySelector('#list');
 addBtn.addEventListener('click', e => {
   e.preventDefault(); //prevent default form behaviour
 
-  let value = task.value; // get task input
-  const newList = document.createElement('li'); //create new list
+  let value = task.value.trim(); // get task input
 
-const delBtn = document.createElement('button');
+  if (value.length > 0){
+    const newList = document.createElement('li'); //create new list
+
+  const delBtn = document.createElement('button');
 
   delBtn.textContent = "Delete";
+  
+  delBtn.classList.add('delBtn');
 
   newList.textContent = value;
 
@@ -25,7 +29,10 @@ const delBtn = document.createElement('button');
   task.value = ''; //reset input value
 
   delBtn.addEventListener('click', e => {
-  newList.remove();
-})
+      newList.remove();
+    })
+  } else{
+    alert('Please add a task')
+  }
 })
 
