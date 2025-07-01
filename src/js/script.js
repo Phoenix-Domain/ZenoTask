b1cb9c3e487da2b904fc862453229a1093dc4b96
Batubo Victory <victorybatubo76@gmail.com>
2025-07-01 13:09:04 +0100

    Prevent automatic form submit behaviour

--- src/js/script.js
+++ src/js/script.js
@@ -8,3 +8,25 @@ const list = document.querySelector('#list');
 
 const listArr = JSON.parse(localStorage.getItem('listArray')) || [];
 
+addBtn.addEventListener('click', addList);
+
+function addList(e){
+
+  e.preventDefault();
+  
+  const taskInput = task.value;
+
+  const newList = document.createElement('li');
+
+  newList.textContent = taskInput;
+
+  const delBtn = document.createElement('button');
+
+  delBtn.textContent = "Delete";
+
+  delBtn.classList.add('delBtn')
+
+  newList.append(delBtn);
+
+  list.append(newList);
+}
