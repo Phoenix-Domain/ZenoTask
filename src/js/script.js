ad6c3b250e47d5556b05f7cc26a5b7d909ac974b
Batubo Victory <victorybatubo76@gmail.com>
2025-07-01 12:51:16 +0100

    Refactor project for localStorage feature

--- src/js/script.js
+++ src/js/script.js
@@ -2,46 +2,9 @@ const body = document.querySelector('body');
 
 const task = document.querySelector('#task');
 
-const addTask = document.querySelector('#addBtn');
+const addBtn = document.querySelector('#addBtn');
 
 const list = document.querySelector('#list');
-;
 
-let listArr = JSON.parse(localStorage.getItem('listArray')) || [];
-
-addBtn.addEventListener('click', e => {
-  e.preventDefault(); //prevent default form behaviour
-
-  let value = task.value.trim(); // get task input
-
-  if (value.length > 0){
-    const newList = document.createElement('li'); //create new list
-
-  const delBtn = document.createElement('button');
-
-  delBtn.textContent = "Delete";
-  
-  delBtn.classList.add('delBtn');
-
-  newList.textContent = value;
-
-  listArr.push(value); // push list into list array
-
-  newList.append(delBtn)
-
-  list.append(newList);
-
-
-  task.value = ''; //reset input value
-
-  delBtn.addEventListener('click', e => {
-      newList.remove();
-    })
-  } else{
-    alert('Please add a task')
-  }
-  localStorage.setItem('listArray', JSON.stringify(listArr))
-})
-
-
+const listArr = JSON.parse(localStorage.getItem('listArray')) || [];
 
