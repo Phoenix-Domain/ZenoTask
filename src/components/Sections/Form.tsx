c7bed65c8114d4e53e1660bab9d3810d1727ca81
Batubo Victory <victorybatubo76@gmail.com>
2025-09-16 16:33:04 +0100

    feat: Put all labels and input into fieldset

--- src/components/Sections/Form.tsx
+++ src/components/Sections/Form.tsx
@@ -4,26 +4,31 @@ import Input from "../FormItems/Input";
 function Form(){
     return(
         <form action="">
-            <div>
-                <Label htmlFor="" content='Title'/>
-                <input type="text" />
-            </div>
-            <div>
-                <Label htmlFor="" content='Due Date' />
-                <input type="text" />
-            </div>
-            <div>
-                <Label htmlFor="" content='Time' />
-                <input type="text" />
-            </div>
-            <div>
-                <Label htmlFor="" content='Priority' />
-                <input type="text" />
-            </div>
-            <div>
-                <Label htmlFor="" content='Add Note' />
-                <textarea name="" id=""></textarea>
-            </div>
+            <fieldset className='border flex flex-col'>
+                <legend className="font-bold">
+                    Add New Task
+                </legend>
+                <div>
+                    <Label htmlFor="" content='Title'/>
+                    <input type="text" />
+                </div>
+                <div>
+                    <Label htmlFor="" content='Due Date' />
+                    <input type="text" />
+                </div>
+                <div>
+                    <Label htmlFor="" content='Time' />
+                    <input type="text" />
+                </div>
+                <div>
+                    <Label htmlFor="" content='Priority' />
+                    <input type="text" />
+                </div>
+                <div>
+                    <Label htmlFor="" content='Add Note' />
+                    <textarea name="" id=""></textarea>
+                </div>
+            </fieldset>
         </form>
     )
 }
