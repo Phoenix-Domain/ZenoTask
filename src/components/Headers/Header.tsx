5396ee29e19a6d13a0072239fb453aff2d0ba366
Batubo Victory <victorybatubo76@gmail.com>
2025-09-16 14:54:28 +0100

    refactor: Use number as type instead of 1-6

--- src/components/Headers/Header.tsx
+++ src/components/Headers/Header.tsx
@@ -3,21 +3,23 @@ interface HeaderProps{
     content: string,
 } //Type for header object and props
 
-type HeaderSize = {
-    size: 1 | 2 | 3 | 4 | 5 | 6;
-} //Type for Header size
 class HeaderObject{
-    size: HeaderSize;
+    size: number;
     content: string;
     headerFontStyle?: string // Create key to hold header fontSize and fontStyle
 
-    constructor(size: HeaderSize, content: string, headerFontStyle?: string){
+    constructor(size: number, content: string, headerFontStyle?: string){
         this.size = size;
         this.content = content;
         this.headerFontStyle = headerFontStyle;
     }
-}// Header object which accepts size, style and content parameters
 
+    setFontStyle(){
+        if(this.size === 1){}
+    } 
+}// Header object which accepts size, style and content parameters
+const testObj = new HeaderObject(1, 'Zenooo')
+console.log(testObj.headerFontStyle);
 
 
 function Header({size, content}: HeaderProps){
