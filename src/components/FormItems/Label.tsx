e048e54fd2564dbb72ecdd571203c3c099bf541a
Batubo Victory <victorybatubo76@gmail.com>
2025-09-16 16:21:53 +0100

    feat: Use map to loop through the props so you can avoid repetiton

--- src/components/FormItems/Label.tsx
+++ src/components/FormItems/Label.tsx
@@ -1,6 +1,14 @@
-function Label(){
+type LabelKeys = 'htmlFor' | 'content' | 'style'; //list of attributes or props
+
+type LabelTypes = {
+    [k in LabelKeys]: string
+}// Use map to avoid repetition since all types are meant to be strings
+
+function Label({ htmlFor, content, style }: LabelTypes){
     return(
-        <label htmlFor=""></label>
+        <label htmlFor={htmlFor} className={style}>
+            {content}
+        </label>
     )
 }
 
