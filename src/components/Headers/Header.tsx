90c7620286417442ffd695363d948397444a9ba3
Batubo Victory <victorybatubo76@gmail.com>
2025-09-16 14:19:02 +0100

    refactor: Remove style from props and Object

--- src/components/Headers/Header.tsx
+++ src/components/Headers/Header.tsx
@@ -1,24 +1,21 @@
 interface HeaderProps{
     size: 1 | 2 | 3 | 4 | 5 | 6 ,
-    style: string,
     content: string
 } //Type for header object and props
 
 class HeaderObject{
-    size: 1 | 2 | 3 | 4 | 5 | 6; 
-    style: string;
+    size: 1 | 2 | 3 | 4 | 5 | 6;
     content: string;
 
     constructor(size: 1 | 2 | 3 | 4 | 5 | 6, style: string, content: string){
         this.size = size;
-        this.style = style;
         this.content = content;
     }
 }// Header object which accepts size, style and content parameters
 
 
 
-function Header(){
+function Header({size, content}){
     return(
         <>
 
