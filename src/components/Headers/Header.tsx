4a0e1f5f0f55145a5845867f5f1be94bd6795423
Batubo Victory <victorybatubo76@gmail.com>
2025-09-16 14:36:31 +0100

    feat: Create headerSize type to reduce redundancy

--- src/components/Headers/Header.tsx
+++ src/components/Headers/Header.tsx
@@ -3,11 +3,14 @@ interface HeaderProps{
     content: string
 } //Type for header object and props
 
-class HeaderObject{
+type HeaderSize = {
     size: 1 | 2 | 3 | 4 | 5 | 6;
+} //Type for Header size
+class HeaderObject{
+    size: HeaderSize;
     content: string;
 
-    constructor(size: 1 | 2 | 3 | 4 | 5 | 6, content: string){
+    constructor(size: HeaderSize, content: string){
         this.size = size;
         this.content = content;
     }
@@ -18,6 +21,9 @@ class HeaderObject{
 function Header({size, content}: HeaderProps){
     const Tag = `h${size}` as keyof HTMLElementTagNameMap;
     
+    const styleHeader = (size: HeaderSize) => {
+
+    }
     return(
         <Tag>
             {content}
