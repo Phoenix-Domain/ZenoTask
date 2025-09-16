d75147fe3e4dae49be71cbd5edc588fef718a1bf
Batubo Victory <victorybatubo76@gmail.com>
2025-09-16 16:41:33 +0100

    refactor: Replace every input tag with Input components

--- src/components/Sections/Form.tsx
+++ src/components/Sections/Form.tsx
@@ -10,19 +10,19 @@ function Form(){
                 </legend>
                 <div>
                     <Label htmlFor="" content='Title'/>
-                    <input type="text" />
+                    <Input type='text' />
                 </div>
                 <div>
                     <Label htmlFor="" content='Due Date' />
-                    <input type="text" />
+                    <Input type='text' />
                 </div>
                 <div>
                     <Label htmlFor="" content='Time' />
-                    <input type="text" />
+                    <Input type='text' />
                 </div>
                 <div>
                     <Label htmlFor="" content='Priority' />
-                    <input type="text" />
+                    <Input type='text' />
                 </div>
                 <div>
                     <Label htmlFor="" content='Add Note' />
