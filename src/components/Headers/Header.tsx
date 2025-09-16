8295b0a77abaaf02bebb64f05b7899d1fd560142
Batubo Victory <victorybatubo76@gmail.com>
2025-09-16 13:58:05 +0100

    feat: Create HeaderProps type

--- src/components/Headers/Header.tsx
+++ src/components/Headers/Header.tsx
@@ -1,7 +1,13 @@
+interface HeaderProps{
+    size: 1 | 2 | 3 | 4 | 5 | 6 ,
+    style: string,
+    content: string
+}
+
 function Header(){
     return(
         <>
-        
+
         </>
     )
 }
