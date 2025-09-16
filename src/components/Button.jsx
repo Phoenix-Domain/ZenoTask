e84c4459d0f462ad7d63848683d4e963297d1bab
Batubo Victory <victorybatubo76@gmail.com>
2025-09-16 15:51:39 +0100

    fix: Change file type from jsx to tsx

--- src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-function Button(){
-    return(
-        <button>
-
-        </button>
-    )
-}
-
-export default Button
---
+++ src/components/Buttons.tsx
@@ -0,0 +1,15 @@
+interface ButtonType{
+    type: 'button' | 'submit' | 'reset'
+    content: string
+    style: string
+}
+
+function Button(){
+    return(
+        <button>
+
+        </button>
+    )
+}
+
+export default Button
