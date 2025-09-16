34af57c5fb42b8bb09a9fc117999fca12cbd530d
Batubo Victory <victorybatubo76@gmail.com>
2025-09-16 16:28:33 +0100

    refactor: Replace individual Label tags with Label components

--- src/components/Sections/Form.tsx
+++ src/components/Sections/Form.tsx
@@ -1,7 +1,29 @@
+import Label from "../FormItems/Label";
+import Input from "../FormItems/Input";
+
 function Form(){
     return(
         <form action="">
-
+            <div>
+                <Label htmlFor="" content='Title'/>
+                <input type="text" />
+            </div>
+            <div>
+                <Label htmlFor="" content='Due Date' />
+                <input type="text" />
+            </div>
+            <div>
+                <Label htmlFor="" content='Time' />
+                <input type="text" />
+            </div>
+            <div>
+                <Label htmlFor="" content='Priority' />
+                <input type="text" />
+            </div>
+            <div>
+                <Label htmlFor="" content='Add Note' />
+                <textarea name="" id=""></textarea>
+            </div>
         </form>
     )
 }
