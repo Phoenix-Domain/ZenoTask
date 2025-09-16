58a183da794239bb5bf2f01cb4ccd4039bac6414
Batubo Victory <victorybatubo76@gmail.com>
2025-09-16 14:10:32 +0100

    style: Add semicolons at the end of each line

--- src/components/Headers/Header.tsx
+++ src/components/Headers/Header.tsx
@@ -5,15 +5,14 @@ interface HeaderProps{
 }
 
 class HeaderObject implements HeaderProps {
-    size: 1 | 2 | 3 | 4 | 5 | 6 
-    style: string
-    content: string
+    size: 1 | 2 | 3 | 4 | 5 | 6; 
+    style: string;
+    content: string;
 
     constructor(size: 1 | 2 | 3 | 4 | 5 | 6, style: string, content: string){
-        this.size = size
-        this.style = style
-        this.content = content
-
+        this.size = size;
+        this.style = style;
+        this.content = content;
     }
 }
 
