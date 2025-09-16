89b3dbc245fe2de6bc81409cd5482749b3e138a2
Batubo Victory <victorybatubo76@gmail.com>
2025-09-16 14:22:19 +0100

    feat: Render h1 Header component with app name

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import Header from './components/Headers/Header'
 function App() {
   return (
     <>
-      
+      <Header 
+        size={1}
+        content='ZenoTask'
+      />
     </>
   )
 }
