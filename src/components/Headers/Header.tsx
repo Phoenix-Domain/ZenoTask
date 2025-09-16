807c2c425aa5abedfc22e425fbb6c3190a3c9948
Batubo Victory <victorybatubo76@gmail.com>
2025-09-16 14:09:35 +0100

    feat: Create Header object

--- src/components/Headers/Header.tsx
+++ src/components/Headers/Header.tsx
@@ -4,6 +4,21 @@ interface HeaderProps{
     content: string
 }
 
+class HeaderObject implements HeaderProps {
+    size: 1 | 2 | 3 | 4 | 5 | 6 
+    style: string
+    content: string
+
+    constructor(size: 1 | 2 | 3 | 4 | 5 | 6, style: string, content: string){
+        this.size = size
+        this.style = style
+        this.content = content
+
+    }
+}
+
+
+
 function Header(){
     return(
         <>
