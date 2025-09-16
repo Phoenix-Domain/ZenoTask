efb515603667963d3ad3ac42c081c6b7e190a2c5
Batubo Victory <victorybatubo76@gmail.com>
2025-09-16 14:16:54 +0100

    style: Add comments to type and object

--- src/components/Headers/Header.tsx
+++ src/components/Headers/Header.tsx
@@ -2,9 +2,9 @@ interface HeaderProps{
     size: 1 | 2 | 3 | 4 | 5 | 6 ,
     style: string,
     content: string
-}
+} //Type for header object and props
 
-class HeaderObject implements HeaderProps {
+class HeaderObject{
     size: 1 | 2 | 3 | 4 | 5 | 6; 
     style: string;
     content: string;
@@ -14,7 +14,7 @@ class HeaderObject implements HeaderProps {
         this.style = style;
         this.content = content;
     }
-}
+}// Header object which accepts size, style and content parameters
 
 
 
