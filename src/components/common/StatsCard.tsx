a51de471bdaa8515d3b8748f13c97f16bc5e5e9c
Batubo Victory <victorybatubo76@gmail.com>
2025-10-14 14:55:42 +0100

    style: Add styling to Stat cards

--- src/components/common/StatsCard.tsx
+++ src/components/common/StatsCard.tsx
@@ -1,15 +1,15 @@
 interface StatsCardProps{
     statTitle: string;
-    statValue?: string;
+    statValue: string;
 }
 
 function StatsCard({ statTitle, statValue }: StatsCardProps){
     return(
-        <article>
+        <article className='bg-gray-100 p-5 rounded-xl border border-gray-300'>
             <p>
                 Total {statTitle}
             </p>
-            <h3>
+            <h3 className='font-bold text-2xl'>
                 {statValue}
             </h3>
         </article>
