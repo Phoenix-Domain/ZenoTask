0163712092b85e0d9fe673e5aa38daad748f1436
Batubo Victory <victorybatubo76@gmail.com>
2025-10-14 15:38:10 +0100

    feat: Map task array and conditionally render it

--- src/components/common/TaskDisplay.tsx
+++ src/components/common/TaskDisplay.tsx
@@ -1,8 +1,30 @@
-function TaskDisplay(){
+
+interface Task{
+    Title: string;
+    Priority: string;
+    Status: string;
+}
+interface TaskDisplayProps{
+    tasks: Task[]
+}
+
+function TaskDisplay({tasks}: TaskDisplayProps){
     return(
-        <>
-            
-        </>
+        <section>
+            {
+                // Conditional statement to render and display tasks
+                tasks.length <= 0 ? <article>
+                    <p>No Tasks Yet.</p>
+                    <h3>Fill form to add Task</h3>
+                </article> : tasks.map(task => (
+                    <article>
+                        <p>{task.Title}</p>
+                        <p>{task.Priority}</p>
+                        <p>{task.Status}</p>
+                    </article>
+                ))
+            }
+        </section>
     )
 }
 
