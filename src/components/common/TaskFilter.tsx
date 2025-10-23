255c3c3798de03d8c2c05786e9490811075e1bc3
Batubo Victory <victorybatubo76@gmail.com>
2025-10-23 16:34:15 +0100

    feat: Add select element to choose what tasks will be filtered

--- src/components/common/TaskFilter.tsx
+++ src/components/common/TaskFilter.tsx
@@ -1,7 +1,17 @@
 function TaskFilter(){
     return(
-        <section>
-            
+        <section className='m-2 border border-gray-400 py-1 px-2 rounded-2xl bg-gray-300'>
+            <article className='text-center'>
+                <label htmlFor='Filter' className=''>
+                    Filter Tasks by:
+                </label>
+                <select id="Filter" className='font-bold ml-3 rounded-2xl bg-blue-700 p-1 text-white outline-0'>
+                    <option value="Select filter">Select filter</option>
+                    <option value="name">Name</option>
+                    <option value="priority">Priority</option>
+                    <option value="status">Status</option>
+                </select>
+            </article>
         </section>
     )
 }
